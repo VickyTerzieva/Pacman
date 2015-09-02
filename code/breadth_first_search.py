b6e7ee3633084a55473f2153d91a79c3003d8911
Vicky <vgt_94@abv.bg>
2015-09-02 15:30:53 +0300

    Making the code more readable

--- code/breadth_first_search.py
+++ code/breadth_first_search.py
@@ -19,21 +19,27 @@ def neighbours(node):
 
 
 def breadth_first_search(start, goal):
+
     visited = [[False for x in range(500)] for y in range(500)]
     queue = Queue()
     queue.put_nowait(start)
     visited[start.x()][start.y()] = True
     current_node = queue.get()
     node_neighbours = neighbours(current_node)
+
     for i in range(node_neighbours.__len__()):
         if visited[node_neighbours[i].x()][node_neighbours[i].y()] \
                 is False:
+            
             queue.put_nowait(node_neighbours[i])
             visited[current_node.x()][current_node.y()] = True
+
     while not current_node.x() == goal.x() \
             and not current_node.y() == goal.y():
+
         current_node = queue.get()
         node_neighbours = neighbours(current_node)
+
         for i in range(node_neighbours.__len__()):
             if visited[node_neighbours[i].x()][node_neighbours[i].y()] \
                     is False:
